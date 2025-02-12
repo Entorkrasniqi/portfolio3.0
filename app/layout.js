5cf2eeb560a3490cbe4c16f91fc30b563ae6f1e1
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2025-02-12 22:38:51 +0200

    update

--- app/layout.js
+++ app/layout.js
@@ -10,9 +10,10 @@ import "./css/globals.scss";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Portfolio of Abu Said - Software Developer",
+  title: "Portfolio of Entor Krasniqi - Software Engineer",
   description:
-    "This is the portfolio of Abu Said. I am a full stack developer and a self taught developer. I love to learn new things and I am always open to collaborating with others. I am a quick learner and I am always looking for new challenges.",
+    "This is the portfolio of Entor Krasniqi. I am studying ict at metropolia university of applied sciences and i am majoring on software engineering and minoring on cybersecurity.",
+
 };
 
 export default function RootLayout({ children }) {
@@ -20,7 +21,7 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body className={inter.className}>
         <ToastContainer />
-        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-white">
+        <main className="min-h-screen relative mx-auto px-6 sm:px-12 lg:max-w-[70rem] xl:max-w-[76rem] 2xl:max-w-[92rem] text-gray-800">
           <Navbar />
           {children}
           <ScrollToTop />
