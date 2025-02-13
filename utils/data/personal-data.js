27ee7c1ff58178ea49cfd04fa84af82f52db5487
Entorkrasniqi <124073124+Entorkrasniqi@users.noreply.github.com>
2025-02-13 11:14:13 +0200

    u

--- utils/data/personal-data.js
+++ utils/data/personal-data.js
@@ -2,7 +2,7 @@ export const personalData = {
   name: "Entor Krasniqi",
   profile: '/profile.png',
   designation: "Software Engineer",
-  description: "My name is Entor Krasniqi. I am a Studying at Metropolia University of applied sciences and majoring as a software engineer also minoring in cybersecurity. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving.",
+  description: "My name is Entor Krasniqi. I am a Studying at Metropolia University of applied sciences and specializing as a software engineer also minoring in cybersecurity. I am a quick learner with a self-learning attitude. I love to learn and explore new technologies and am passionate about problem-solving.",
   email: 'entorkrasniqi99@gmail.com',
   phone: '044 242 9911',
   address: 'Helsinki, Finland ',
